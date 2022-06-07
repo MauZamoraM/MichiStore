fc1e8708fa121ed3e6db398a5c2eb758f00d4e4a
Oscar <oscar.7n7@gmail.com>
2022-06-07 12:27:12 -0500

    Update Cart.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,7 +3,9 @@ import React from 'react';
 export const Cart = ({ titulo }) => {
 	return (
 		<div>
-			<h1 className="mt-3">{titulo}</h1>
+			<h1 className="mt-4" style={{ color: '#fab43d' }}>
+				- {titulo} -
+			</h1>
 		</div>
 	);
 };
