c13dda7a770304fbaccf67ad2aa136d9561250ba
Oscar <orodriguezc1603@alumno.ipn.mx>
2022-06-22 01:09:15 -0500

    Actualizacion texto

--- src/components/Inicio/Inicio.js
+++ src/components/Inicio/Inicio.js
@@ -1,10 +1,11 @@
-import imgB from './Welcomee.png';
+import imgB from './Welcome.png';
 import './Inicio.css';
 
 export function Inicio() {
 	return (
 		<div className="incio-container animate__animated animate__fadeIn">
 			<img className="bienvenido" src={imgB} alt="Img" />
+			<br />
 		</div>
 	);
 }
